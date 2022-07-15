a325d156f2e66070a7688f60e0e40aa5fa2307d2
goeswiner <ogoesdiego@gmail.com>
2022-07-15 18:38:24 -0300

    added project using modules - calculator

---
+++ exercicios-JS/projeto-modulos-calculadora/app.js
@@ -0,0 +1,9 @@
+var funSomar = require("./operacoes/soma.js")
+var funSubtrair = require("./operacoes/subtrai.js")
+var funMultiplicar = require("./operacoes/multiplica.js")
+var funDividir = require("./operacoes/divide.js")
+
+console.log(funSomar(1,2))
+console.log(funSubtrair(5,2))
+console.log(funMultiplicar(2,2))
+console.log(funDividir(25,5))
